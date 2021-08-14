1d5f7f6da510d4edc64fa8af03716672f9a77354
rahat14 <rahatshovo6666@gmail.com>
2021-08-14 15:49:58 +0600

    added Some New Designs

---
+++ app/src/main/java/com/metacoder/epbpos/ui/ProductEditPage.java
@@ -0,0 +1,16 @@
+package com.metacoder.epbpos.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.metacoder.epbpos.R;
+
+public class ProductEditPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_edit_page);
+    }
+}
